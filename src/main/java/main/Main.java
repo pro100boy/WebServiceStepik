f0e48631991534f550e31fdf4f014c1c8130a4bf
Pavel <gpg@ua.fm>
2016-12-05 17:26:16 +0200

    msg

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,15 +8,9 @@ import servlets.AllRequestsServlet;
 /**
  * @author v.chibrikov
  *         <p>
- *         Пример кода для курса на https://stepic.org/
+ *         РџСЂРёРјРµСЂ РєРѕРґР° РґР»СЏ РєСѓСЂСЃР° РЅР° https://stepic.org/
  *         <p>
- *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
- *
- *         Задача:
-Написать сервлет, который будет обрабатывать запросы на /mirror
-При получении GET запроса с параметром key=value сервлет должен вернуть в response строку содержащую value.
-Например, при GET запросе /mirror?key=hello сервер должен вернуть страницу, на которой есть слово "hello".
-
+ *         РћРїРёСЃР°РЅРёРµ РєСѓСЂСЃР° Рё Р»РёС†РµРЅР·РёСЏ: https://github.com/vitaly-chibrikov/stepic_java_webserver
  */
 public class Main {
     public static void main(String[] args) throws Exception {
